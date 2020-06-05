3ff859953ee854e8847f8186ba848267e03a7118
Christopher Lenz <lenz@mediatis.de>
2020-06-05 16:15:13 +0200

    Create widget DOM node lazily as recommended by the docs (https://prosemirror.net/docs/ref/#view.Decoration%5Ewidget).
    See https://discuss.prosemirror.net/t/issues-with-marks-when-using-prosemirror-invisibles/2779/5

--- src/js/utils/create-deco.js
+++ src/js/utils/create-deco.js
@@ -1,11 +1,17 @@
 import { Decoration } from 'prosemirror-view';
 
 export default (pos, type) => {
-  const span = document.createElement('span');
-  span.classList.add('invisible');
-  span.classList.add(`invisible--${type}`);
-  return Decoration.widget(pos, span, {
-    marks: [],
-    key: type
-  });
+  return Decoration.widget(
+    pos,
+    () => {
+      const span = document.createElement('span');
+      span.classList.add('invisible');
+      span.classList.add(`invisible--${type}`);
+      return span;
+    },
+    {
+      marks: [],
+      key: type
+    }
+  );
 };
